248eb1b2924523a866134aef2058a272974bf6b4
Filipe <fstraubel10@gmail.com>
2025-09-12 17:43:36 -0300

    Criado Controller para detalhes dos veiculos

---
+++ web-api/Controllers/DetalhesVeiculoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GestaoDeEstacionamento.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DetalhesVeiculoController : Controller
+    {
+        private readonly ILogger<DetalhesVeiculoController> _logger;
+
+        public DetalhesVeiculoController(ILogger<DetalhesVeiculoController> logger)
+        {
+            _logger = logger;   
+        }
+
+    }
+}
